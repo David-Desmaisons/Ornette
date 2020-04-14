d49bc59158800f72076a6a8d264de672744b676b
David <desmaisons_david@hotmail.com>
2020-04-14 00:42:35 -0300

    WIP player

--- Ornette.Application/ViewModel/PlayerViewModel.cs
+++ Ornette.Application/ViewModel/PlayerViewModel.cs
@@ -5,10 +5,18 @@ namespace Ornette.Application.ViewModel
 {
     public class PlayerViewModel : Neutronium.BuildingBlocks.ViewModel
     {
+        private readonly IMusicPlayer _MusicPlayer;
         public ObservableCollection<string> Tracks { get; }
 
-        public PlayerViewModel(IEnumerable<string> pathsToMusic)
+        public double Volume
         {
+            get => _MusicPlayer.Volume;
+            set => _MusicPlayer.Volume = value;
+        }
+
+        public PlayerViewModel(IMusicPlayer musicPlayer, IEnumerable<string> pathsToMusic)
+        {
+            _MusicPlayer = musicPlayer;
             Tracks = new ObservableCollection<string>(pathsToMusic);
         }
     }

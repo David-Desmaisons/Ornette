52e3d33676208c78831bc8e6463bc6cc76f11cd8
David <desmaisons_david@hotmail.com>
2020-04-19 11:07:14 -0300

    :lipstick: Adding simple player component

---
+++ Ornette/View/src/components/player/simplePlayer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+    <icon-button icon="mdi-play"  :command="player.Play"></icon-button>
+    <icon-button icon="mdi-pause" :command="player.Pause"></icon-button>
+    <icon-button icon="mdi-stop"  :command="player.Stop"></icon-button>
+  </div>
+</template>
+
+<script>
+import iconButton from "../iconButton";
+
+export default {
+  props: {
+    player: {
+      required: true,
+      type: Object
+    }
+  },
+  components: {
+    iconButton
+  }
+};
+</script>
+
+<style>
+</style>
--- Ornette/View/src/pages/main.vue
+++ Ornette/View/src/pages/main.vue
@@ -3,23 +3,31 @@
     <v-content>
       <v-container fluid class="main-container">
         <v-layout>
+          <simplePlayer :player="viewModel.Player" />
           <v-flex xs12 sm6 offset-sm3>
             <v-card>
             </v-card>
           </v-flex>
         </v-layout>
       </v-container>
+
+      
     </v-content>
   </main>
 </template>
 
 <script>
+import simplePlayer from "../components/player/simplePlayer";
+
 const props = {
   viewModel: Object
 };
 
 export default {
-  props
+  props,
+  components:{
+    simplePlayer
+  }
 };
 </script>
 

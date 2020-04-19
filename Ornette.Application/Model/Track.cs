d845e0d89f43b871e4a2035d89cf5a9bc651569d
David <desmaisons_david@hotmail.com>
2020-04-19 16:22:31 -0300

    Improving track meta data

--- Ornette.Application/Model/Track.cs
+++ Ornette.Application/Model/Track.cs
@@ -3,7 +3,6 @@
     public class Track
     {
         public string Path { get; }
-
         public TrackMetaData MetaData { get; }
 
         public Track(string path, TrackMetaData metaData)
--- Ornette.Application/Model/TrackMetaData.cs
+++ Ornette.Application/Model/TrackMetaData.cs
@@ -1,14 +1,18 @@
-﻿namespace Ornette.Application.Model
+﻿using System;
+
+namespace Ornette.Application.Model
 {
     public class TrackMetaData
     {
-        public TrackMetaData(string name, int? trackNumber = null)
+        public TrackMetaData(string name, TimeSpan? duration = null, int? trackNumber = null)
         {
             TrackNumber = trackNumber;
             Name = name;
+            Duration = duration;
         }
 
         public int? TrackNumber { get; }
         public string Name { get; }
+        public TimeSpan? Duration { get; }
     }
 }

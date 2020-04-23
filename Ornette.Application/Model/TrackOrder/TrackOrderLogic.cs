7b4cab22adc0a39186e1cab08b20e5b2e89ae8b5
David <desmaisons_david@hotmail.com>
2020-04-23 11:06:12 -0300

    WIP track order logic

---
+++ Ornette.Application/Model/TrackOrder/TrackOrderLogic.cs
@@ -0,0 +1,10 @@
+﻿namespace Ornette.Application.Model.TrackOrder
+{
+    internal static class TrackOrderLogic
+    {
+        internal static ITrackOrderLogic GetLogic(bool random)
+        {
+            return new LinearTrackOrderLogic();
+        }
+    }
+}

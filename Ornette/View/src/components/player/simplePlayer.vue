abb44563fc85f41e1727229891e44096b8181425
David <desmaisons_david@hotmail.com>
2020-04-26 00:23:40 -0300

    Improving player

--- Ornette/View/src/components/player/simplePlayer.vue
+++ Ornette/View/src/components/player/simplePlayer.vue
@@ -1,28 +1,43 @@
 <template>
   <div class="simple-player">
-
     <p class="time">{{ player.PositionInSeconds | formatTime }}</p>
 
     <marquee class="track-name">{{ player.CurrentTrack | track }}</marquee>
-
-    <icon-button icon="mdi-skip-backward" :command="player.Back" />
-
-    <icon-button icon="mdi-play" :command="player.Play" />
-
-    <icon-button icon="mdi-pause" :command="player.Pause" />  
-
-    <icon-button icon="mdi-stop" :command="player.Stop" />
-
-    <icon-button icon="mdi-skip-forward" :command="player.Next" />
 
     <v-slider
       v-if="player.CurrentTrack"
       v-model="player.PositionInSeconds"
       :max="player.CurrentTrack | totalSeconds"
-      class="full"
+      class="full slider"
       :dense="true"
     />
-    
+
+    <v-btn-toggle class="control">
+      <icon-button icon="mdi-skip-backward" :command="player.Back" />
+
+      <icon-button icon="mdi-play" :command="player.Play" />
+
+      <icon-button icon="mdi-pause" :command="player.Pause" />
+
+      <icon-button icon="mdi-stop" :command="player.Stop" />
+
+      <icon-button icon="mdi-skip-forward" :command="player.Next" />
+    </v-btn-toggle>
+
+    <v-btn-toggle
+      class="full-rigth"
+      multiple
+      :value="toogleValue"
+      @change="changeToogle"
+    >
+      <v-btn small icon>
+        <v-icon>mdi-shuffle-variant</v-icon>
+      </v-btn>
+
+      <v-btn small icon>
+        <v-icon>mdi-repeat</v-icon>
+      </v-btn>
+    </v-btn-toggle>
   </div>
 </template>
 
@@ -38,17 +53,32 @@ export default {
   },
   components: {
     iconButton
+  },
+  computed: {
+    toogleValue() {
+      const res = [];
+      if (this.player.AutoReplay) res.push(1);
+      if (this.player.RandomPlay) res.push(0);
+      return res;
+    }
+  },
+  methods: {
+    changeToogle(evt) {
+      this.player.AutoReplay = evt.includes(1);
+      this.player.RandomPlay = evt.includes(0);
+    }
   }
 };
 </script>
 
 <style scoped="true">
 .simple-player {
-  width: 100px;
+  width: 200px;
   height: 50px;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: 20px 1fr auto;
+  grid-template-columns: 2fr 3fr 2fr;
+  grid-template-rows: 25px 30px 1fr;
+  grid-column-gap: 20px;
 }
 
 .simple-player div {
@@ -56,17 +86,16 @@ export default {
   place-self: center;
 }
 
-.simple-player .full {
-  grid-column: 1 / span 5;
+.simple-player .control {
+  grid-column: 1 / span 2;
   align-self: center;
   place-self: center;
-  width: 100%;
 }
 
 .simple-player .time {
-  grid-column: 1 / span 2;
+  grid-column: 1 / span 1;
   margin-bottom: 0;
-  font-size: 18px;
+  font-size: 20px;
   text-align: center;
   align-self: center;
   place-self: center;
@@ -76,9 +105,17 @@ export default {
   font-size: 10px;
 }
 
+.simple-player .full.slider {
+  height: 25px;
+  width: 100%;
+  grid-column: 1 / span 3;
+  align-self: center;
+  place-self: center;
+}
+
 .simple-player .track-name {
   width: 100%;
-  grid-column: 3 / span 3;
+  grid-column: 2 / span 2;
   align-self: center;
   place-self: center;
 }

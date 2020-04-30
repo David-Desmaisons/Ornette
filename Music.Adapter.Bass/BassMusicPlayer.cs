06f9f7929a1caff661e19d6f88230a3c3cb1d5f4
David <desmaisons_david@hotmail.com>
2020-04-29 23:44:43 -0300

    Improving  bass music

--- Music.Adapter.Bass/BassMusicPlayer.cs
+++ Music.Adapter.Bass/BassMusicPlayer.cs
@@ -6,43 +6,71 @@ namespace Music.Adapter.Bass
 {
     public class BassMusicPlayer : IMusicPlayer
     {
+        private readonly string _Email;
+        private readonly string _Password;
+
         private static bool Is64Bits => (IntPtr.Size == 8);
         private static string Path => Is64Bits ? "x64" : "x86";
 
         private static BassMusicPlayer _BassMusicPlayer = null;
 
+        private BassMusicPlayer(string email, string password)
+        {
+            _Email = email;
+            _Password = password;
+        }
+
+        private BassMusicPlayer Init()
+        {
+            BassNet.Registration(_Email, _Password);
+            Un4seen.Bass.Bass.LoadMe(Path);
+
+            if (!Un4seen.Bass.Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero))
+                throw new Exception("Not possible create ");
+
+            return this;
+        }
+
         public double Volume
         {
             get => Un4seen.Bass.Bass.BASS_GetConfig(BASSConfig.BASS_CONFIG_GVOL_STREAM) / 100d;
-            set => Un4seen.Bass.Bass.BASS_SetConfig(BASSConfig.BASS_CONFIG_GVOL_STREAM, (int)(Math.Truncate(value * 100)));
+            set => Un4seen.Bass.Bass.BASS_SetConfig(BASSConfig.BASS_CONFIG_GVOL_STREAM,
+                (int)(Math.Truncate(value * 100)));
         }
-
-        private BassMusicPlayer() { }
 
         public static IMusicPlayer Init(string email, string password)
         {
             if (_BassMusicPlayer != null)
                 return _BassMusicPlayer;
 
-            Un4seen.Bass.BassNet.Registration(email, password);
-            Un4seen.Bass.Bass.LoadMe(Path);
-
-            if (!Un4seen.Bass.Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero))
-                throw new Exception("Not possible create ");
-
-            return _BassMusicPlayer = new BassMusicPlayer(); ;
+            return _BassMusicPlayer = new BassMusicPlayer(email, password).Init();
         }
 
         public void Dispose()
         {
-            Un4seen.Bass.Bass.BASS_Free();
+            Release();
             _BassMusicPlayer = null;
+        }
+
+        private void Release()
+        {
+            Un4seen.Bass.Bass.BASS_Free();
+        }
+
+        private void RestartBassIfNeeded()
+        {
+            if (Un4seen.Bass.Bass.BASS_IsStarted())
+                return;
+
+            Release();
+            Init();
         }
 
         public ITrackPlayer CreateTrackPlayer(string path)
         {
+            RestartBassIfNeeded();
             var stream = Un4seen.Bass.Bass.BASS_StreamCreateFile(path, 0, 0, BASSFlag.BASS_DEFAULT);
-            return (stream == 0) ? BrokenTrackPlayer .Instance : new BassTrackPlayer(stream);
+            return (stream == 0) ? BrokenTrackPlayer.Instance : new BassTrackPlayer(stream);
         }
     }
 }

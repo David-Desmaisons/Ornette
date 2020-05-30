a819f1f3f339e67a3432fb5df556a990179225db
David <desmaisons_david@hotmail.com>
2020-05-30 00:12:45 -0300

    Adding album displayer

---
+++ Ornette/View/src/components/album/albumDisplayer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img v-if="currentImageUri" :src="currentImageUri" />
+    <slot v-else name="no-art" />
+  </div>
+</template>
+<script>
+export default {
+  name: "albumDisplayer",
+  props: {
+    album: {
+      required: true,
+      type: Object
+    }
+  },
+  data() {
+    return {
+      imageIndex: 0
+    };
+  },
+  computed: {
+    currentImageUri() {
+      const {
+        album: { Images },
+        imageIndex
+      } = this;
+      return Images.length > 0 ? Images[imageIndex].Uri : null;
+    }
+  }
+};
+</script>
+<style lang="sass" scoped></style>

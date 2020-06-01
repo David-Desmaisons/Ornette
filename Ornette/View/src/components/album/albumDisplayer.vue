a630782ffbbd1e61af6d0de5fbe7eae10eea7491
David <desmaisons_david@hotmail.com>
2020-06-01 01:20:22 -0300

    Improving album displayer

--- Ornette/View/src/components/album/albumDisplayer.vue
+++ Ornette/View/src/components/album/albumDisplayer.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <img v-if="currentImageUri" :src="currentImageUri" />
-    <slot v-else name="no-art" />
+  <div v-if="album" class="album-display">
+    <img
+      class="main main-image"
+      v-if="currentImageUri"
+      :src="currentImageUri"
+    />
+    <slot class="main" v-else name="no-art" />
+
+    <span class="album-artist">{{ album.Artists | join }}</span>
+
+    <span class="album-title">{{ album.Name }}</span>
   </div>
 </template>
 <script>
@@ -9,7 +17,6 @@ export default {
   name: "albumDisplayer",
   props: {
     album: {
-      required: true,
       type: Object
     }
   },
@@ -29,4 +36,35 @@ export default {
   }
 };
 </script>
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+.album-display
+  display: grid
+  grid-template-rows: 1fr auto auto
+  grid-template-columns: auto
+  width: fit-content
+  height: fit-content
+  margin: 2px
+
+  .main
+    width: 150px
+
+    &.main-image
+      text-align: center
+      display: block
+      margin-left: auto
+      margin-right: auto
+      border-radius: 2%
+
+  span
+    text-align: center
+
+    &.album-artist
+      font-weight: bold
+      font-size: 16px
+      align-self: center
+
+    &.album-title
+      font-style: italic
+      font-size: 12px
+      align-self: center
+</style>

96ac345015fc50080e951e715717acaab08c52ab
David <desmaisons_david@hotmail.com>
2020-06-18 23:11:22 -0300

    Implement short play play-pause logic

--- Ornette/View/src/components/player/playControl/shortPlay.vue
+++ Ornette/View/src/components/player/playControl/shortPlay.vue
@@ -2,7 +2,12 @@
   <div class="play">
     <icon-button icon="mdi-skip-backward" :command="player.Back" fab small />
 
-    <icon-button icon="mdi-play" :command="player.Play" fab :small="false"/>
+    <icon-button
+      :icon="isPlaying ? 'mdi-pause' : 'mdi-play'"
+      :command="isPlaying ? player.Pause : player.Play"
+      fab
+      :small="false"
+    />
 
     <icon-button icon="mdi-skip-forward" :command="player.Next" fab small />
   </div>
@@ -10,11 +15,16 @@
 
 <script>
 export default {
-  name: "complete-play",
+  name: "short-play",
   props: {
     player: {
       required: true,
       type: Object
+    }
+  },
+  computed: {
+    isPlaying() {
+      return this.player.State.intValue === 4;
     }
   }
 };

9fa64bfb3b40cfb90f628599d7ab6ae56e4a3577
David <desmaisons_david@hotmail.com>
2020-06-22 12:18:41 -0300

    Making player VM singleton

--- Ornette/App_Start/DependencyInjectionConfiguration.cs
+++ Ornette/App_Start/DependencyInjectionConfiguration.cs
@@ -12,6 +12,7 @@ using Ornette.Application.Io;
 using Ornette.Application.Model;
 using Ornette.Application.Model.TrackOrder;
 using Ornette.Application.MusicPlayer;
+using Ornette.Application.ViewModel;
 using Ornette.IO;
 using Ornette.UI.ViewModel.Pages;
 
@@ -61,6 +62,7 @@ namespace Ornette.UI
             kernel.Bind<IDispatcher>().ToConstant(new WPFUIDispatcher(window.Dispatcher));
             kernel.Bind<IApplicationLifeCycle>().To<ApplicationLifeCycle>();
             kernel.Bind<MainViewModel>().ToSelf().InSingletonScope();
+            kernel.Bind<PlayerViewModel>().ToSelf().InSingletonScope();
             kernel.Bind<IPlayer>().To<Player>().InSingletonScope();
             kernel.Bind<IIoReader>().To<IoReader>().InSingletonScope();
             kernel.Bind<ITrackOrderLogicFactory>().To<TrackOrderLogicFactory>().InSingletonScope();

5b7d16b1bbadd950181df7d76e90385bb9c3b337
David <desmaisons_david@hotmail.com>
2020-12-02 09:44:45 -0300

    WIP

--- Ornette.Application/Converter/ConvertedFile.cs
+++ Ornette.Application/Converter/ConvertedFile.cs
@@ -1,14 +1,18 @@
-﻿namespace Ornette.Application.Converter
+﻿using Ornette.Application.Model.Descriptions;
+
+namespace Ornette.Application.Converter
 {
     public class ConvertedFile
     {
-        public ConvertedFile(string path, IConvertContext context)
+        public ConvertedFile(string path, TrackDescription description, IConvertContext context)
         {
             Path = path;
             Context = context;
+            Description = description;
         }
 
         public string Path { get; }
+        public TrackDescription Description { get; }
         public IConvertContext Context { get; }
     }
 }

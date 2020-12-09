d972411215f7a1f8d272bbb27ae52149ca4fedf2
David <desmaisons_david@hotmail.com>
2020-12-08 23:35:32 -0300

    WIP

--- Music.Adapter.Bass/Converter/Mp3Converter.cs
+++ Music.Adapter.Bass/Converter/Mp3Converter.cs
@@ -2,13 +2,28 @@
 using Ornette.Application.Converter.Command;
 using System;
 using System.Threading;
+using Music.Adapter.Bass.Core;
+using Un4seen.Bass;
 
 namespace Music.Adapter.Bass.Converter
 {
-    public class Mp3Converter : IMusicConverter<Mp3ConverterCommand>
+    public class Mp3Converter : IMusicConverter<Mp3FolderConverterCommand>
     {
-        public IObservable<ConvertedFile> Convert(Mp3ConverterCommand command, CancellationToken token = default, IProgress<IConvertUpdate> progress = null)
+        private readonly ISessionManager _SessionManager;
+
+        public Mp3Converter(ISessionManager sessionManager)
         {
+            _SessionManager = sessionManager;
+        }
+
+
+        public IObservable<ConvertedFile> Convert(Mp3FolderConverterCommand command, CancellationToken token = default(CancellationToken), IProgress<IConvertUpdate> progress = null)
+        {
+            _SessionManager.Restart();
+            var a = new Un4seen.Bass.Misc.EncoderMP3(1);
+            var b = new Un4seen.Bass.Misc.EncoderLAME(1);
+
+
             throw new NotImplementedException();
         }
     }

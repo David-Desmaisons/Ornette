374af68b128254bcea8e03078ead2a1fe1d5d71d
Desmaisons <desmaisons_david@hotmail.com>
2020-12-21 17:59:51 -0300

    Improving tests

---
+++ Tests/Ornette.Application.Tests/Integration/Cue/CueParserTest.cs
@@ -0,0 +1,26 @@
+﻿using Ornette.Application.Integration.Cue;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace Ornette.Application.Tests.Integration.Cue
+{
+    public class CueParserTest
+    {
+        private readonly CueParser _CueParser = new CueParser();
+        private readonly List<string[]> _CueContents;
+
+        public CueParserTest()
+        {
+            _CueParser = new CueParser();
+            _CueContents = new[] { "Andrew Hill - Andrew!!!", "Evolution", "Tim Berne - Fractured Fairy Tales" }.Select(name => File.ReadAllLines($"\\Resources\\{name}.cue")).ToList();
+        }
+
+        [Fact]
+        public void Parse_parses_FileName()
+        {
+
+        }
+    }
+}

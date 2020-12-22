7c9d35018a0ab2297f0196fc0abeb54be9fce0b6
David <desmaisons_david@hotmail.com>
2020-12-21 23:44:42 -0300

    Auto stash before merge of "add-convertion" and "origin/add-convertion"

--- Tests/Ornette.Application.Tests/Integration/Cue/CueParserTest.cs
+++ Tests/Ornette.Application.Tests/Integration/Cue/CueParserTest.cs
@@ -8,13 +8,14 @@ namespace Ornette.Application.Tests.Integration.Cue
 {
     public class CueParserTest
     {
-        private readonly CueParser _CueParser = new CueParser();
+        private readonly CueParser _CueParser;
         private readonly List<string[]> _CueContents;
 
         public CueParserTest()
         {
             _CueParser = new CueParser();
-            _CueContents = new[] { "Andrew Hill - Andrew!!!", "Evolution", "Tim Berne - Fractured Fairy Tales" }.Select(name => File.ReadAllLines($"\\Resources\\{name}.cue")).ToList();
+            _CueContents = new[] { "Andrew Hill - Andrew!!!", "Evolution", "Tim Berne - Fractured Fairy Tales" }
+                .Select(name => File.ReadAllLines($".\\Integration\\Cue\\Resources\\{name}.cue")).ToList();
         }
 
         [Fact]

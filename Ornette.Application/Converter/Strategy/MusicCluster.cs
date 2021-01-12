82b24221e506c3b912aeeeb5ab62356065b6628e
Desmaisons <desmaisons_david@hotmail.com>
2021-01-12 18:44:51 -0300

    Adding music cluster

---
+++ Ornette.Application/Converter/Strategy/MusicCluster.cs
@@ -0,0 +1,18 @@
+﻿namespace Ornette.Application.Converter.Strategy
+{
+    public class MusicCluster
+    {
+        public string[] MusicFiles { get; }
+        public string[] ImageFiles { get; }
+        public string MainFolder { get; }
+        public bool IsLossless { get; }
+
+        public MusicCluster(string mainFolder, string[] musicFiles, string[] imageFiles, bool isLossless)
+        {
+            MainFolder = mainFolder;
+            MusicFiles = musicFiles;
+            ImageFiles = imageFiles;
+            IsLossless = isLossless;
+        }
+    }
+}
